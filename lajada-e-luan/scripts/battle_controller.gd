039658ff4c8861ab71eaa272adf70a13f8e98938
Luan Braga <luancarlosalm@gmail.com>
2026-03-29 01:41:59 -0300

    Update battle_controller.gd
    Added constants to reduce magic numbers

--- lajada-e-luan/scripts/battle_controller.gd
+++ lajada-e-luan/scripts/battle_controller.gd
@@ -1,30 +1,36 @@
 extends Node2D
 
-var player_hp = 10
-var enemy_hp = 10
-var player_guarding = false
-var battle_over = false
+const PLAYER_ATTACK_DAMAGE := 3
+const ENEMY_ATTACK_DAMAGE := 3
+const GUARDED_DAMAGE := 1
+const STARTING_HP := 10
+
+var player_hp: int = STARTING_HP
+var enemy_hp: int = 10
+var player_guarding: bool = false
+var battle_over: bool = false
 
 @onready var player_hp_label = $UI/PlayerHPLabel
 @onready var enemy_hp_label = $UI/EnemyHPLabel
 @onready var status_label = $UI/StatusLabel
 @onready var attack_button = $UI/AttackButton
 @onready var guard_button = $UI/GuardButton
+@onready var reset_button = $UI/ResetButton
 
-func _ready():
+func _ready() -> void:
 	reset_battle()
-	update_ui()
 
-func update_ui():
+func update_ui() -> void:
 	player_hp_label.text = "Player HP: " + str(player_hp)
 	enemy_hp_label.text = "Enemy HP: " + str(enemy_hp)
 
-func player_attack():
+func player_attack() -> void:
 	if battle_over:
 		return
 
-	enemy_hp -= 3
-	# Updates directly into the HP Label
+	enemy_hp -= PLAYER_ATTACK_DAMAGE
+	enemy_hp = max(enemy_hp, 0)
+
 	status_label.text = "You attacked!"
 	update_ui()
 
@@ -34,7 +40,7 @@ func player_attack():
 
 	enemy_turn()
 
-func player_guard():
+func player_guard() -> void:
 	if battle_over:
 		return
 
@@ -42,17 +48,19 @@ func player_guard():
 	status_label.text = "You guarded!"
 	enemy_turn()
 
-func enemy_turn():
+func enemy_turn() -> void:
 	if battle_over:
 		return
 
-	var damage = 3
+	var damage: int = ENEMY_ATTACK_DAMAGE
 
 	if player_guarding:
-		damage = 1
+		damage = GUARDED_DAMAGE
 		player_guarding = false
 
 	player_hp -= damage
+	player_hp = max(player_hp, 0)
+
 	status_label.text = "Enemy attacked for " + str(damage) + " damage!"
 	update_ui()
 
@@ -62,36 +70,31 @@ func enemy_turn():
 
 	status_label.text = "Player Turn"
 
-func end_battle(result_text):
+func end_battle(result_text: String) -> void:
 	battle_over = true
 	status_label.text = result_text
 	attack_button.disabled = true
 	guard_button.disabled = true
 
-
-#Reset function
-
-func reset_battle():
+func reset_battle() -> void:
 	player_hp = 10
 	enemy_hp = 10
 	player_guarding = false
 	battle_over = false
+
 	status_label.text = "Player Turn"
 	attack_button.disabled = false
 	guard_button.disabled = false
-	
-	update_ui()
-	
 
-#Button related stuff
-func _on_attack_button_pressed():
+	update_ui()
+
+func _on_attack_button_pressed() -> void:
 	print("Attack button pressed")
 	player_attack()
 
-func _on_guard_button_pressed():
+func _on_guard_button_pressed() -> void:
 	print("Guard button pressed")
 	player_guard()
-
 
 func _on_reset_button_pressed() -> void:
 	reset_battle()
