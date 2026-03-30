9d01ca188cd84ffb714f1764b38a1d4955df8d93
Luan Braga <luancarlosalm@gmail.com>
2026-03-29 21:08:13 -0300

    First character implementation
    Added initial character kit, with missing pieces

--- lajada-e-luan/scripts/battle/battle_controller.gd
+++ lajada-e-luan/scripts/battle/battle_controller.gd
@@ -1,4 +1,6 @@
 extends Node2D
+# Esse export aqui que vai pegar do player data
+@export var player_data: CharacterData
 
 #Texture
 const PLAYER_ALIVE_TEXTURE = preload("res://assets/characters/Sexo2_player.png")
@@ -6,28 +8,26 @@ const PLAYER_DEAD_TEXTURE = preload("res://assets/characters/heroicaido.png")
 const ENEMY_ALIVE_TEXTURE = preload("res://assets/characters/Sexo2_enemy.png")
 const ENEMY_DEAD_TEXTURE = preload("res://assets/characters/enemy_cuiado.png")
 
-const PLAYER_ATTACK_DAMAGE := 3
+
 const ENEMY_ATTACK_DAMAGE := 3
 const GUARDED_DAMAGE := 1
-const STARTING_HP := 10
-const STARTING_MANA := 10
-const FIREBAll_DAMAGE := 5
-const FIREBALL_MANA_COST := 2
 
-var player_hp: int = STARTING_HP
+
+var player_hp: int
+var player_energy: int = 0
 var enemy_hp: int = 10
 var player_guarding: bool = false
 var battle_over: bool = false
-var player_mana: int = 10
+var overload_cooldown_remaining: int = 0
 
 @onready var player_hp_label = $UI/PlayerHPLabel
 @onready var enemy_hp_label = $UI/EnemyHPLabel
 @onready var status_label = $UI/StatusLabel
-@onready var mana_label = $UI/ManaLabel
-@onready var attack_button = $UI/AttackButton
-@onready var guard_button = $UI/GuardButton
+@onready var energy_label = $UI/EnergyLabel
+@onready var pulse_button = $UI/PulseButton
+@onready var stabilize_button = $UI/StabilizeButton
 @onready var reset_button = $UI/ResetButton
-@onready var skill_button = $UI/SkillButton
+@onready var overload_button = $UI/OverloadButton
 @onready var player_sprite = $UI/PlayerSprite
 @onready var enemy_sprite = $UI/EnemySprite
 @onready var fireball_sprite = $UI/FireballSprite
@@ -51,16 +51,21 @@ func _ready() -> void:
 func update_ui() -> void:
 	player_hp_label.text = "Player HP: " + str(player_hp)
 	enemy_hp_label.text = "Enemy HP: " + str(enemy_hp)
-	mana_label.text = "Mana: " + str(player_mana)
-
-func player_attack() -> void:
+	energy_label.text = "Energy Level: " + str(player_data.max_energy)
+	
+	if overload_cooldown_remaining > 0:
+		overload_button.text = "Overload (" + str(overload_cooldown_remaining) + ")"
+	else:
+		overload_button.text = "Overload"
+		
+func use_pulse() -> void:
 	if battle_over:
 		return
 
-	enemy_hp -= PLAYER_ATTACK_DAMAGE
+	enemy_hp -= player_data.pulse_damage
 	enemy_hp = max(enemy_hp, 0)
 
-	status_label.text = "You attacked!"
+	status_label.text = "You used Pulse!"
 	update_ui()
 
 	if enemy_hp <= 0:
@@ -71,51 +76,47 @@ func player_attack() -> void:
 
 	enemy_turn()
 	
-func player_skill_fireball() -> void:
-	
-	if battle_over:
-		return
-		
-	if player_mana < 2:
-		status_label.text = "You don't have enough mana!"
-		return
-	
-	#Skill went through
-	enemy_hp -= FIREBAll_DAMAGE
-	enemy_hp = max(enemy_hp, 0)
-	player_mana -= FIREBALL_MANA_COST
-	
-	status_label.text = "You used your skill!"
-	
-	update_ui()
-	
-	#Await, espera acabar a funcao ai blabla
-	await play_fireball_animation()
-	
-	if enemy_hp <= 0:
-		set_enemy_dead()
-		end_battle("Blazing Victory!")
-		
-		return
-	
-func play_fireball_animation() -> void:
-	fireball_sprite.visible = true
-	fireball_sprite.global_position = player_sprite.global_position
 
-	var tween = create_tween()
-	tween.tween_property(fireball_sprite, "global_position", enemy_sprite.global_position, 0.8)
-
-	await tween.finished
-	fireball_sprite.visible = false
-
-func player_guard() -> void:
+func use_stabilize() -> void:
 	if battle_over:
 		return
 
 	player_guarding = true
-	status_label.text = "You guarded!"
+	status_label.text = "You used Stabilize!"
+	
 	enemy_turn()
 
+func update_cooldowns() -> void:
+	
+	if overload_cooldown_remaining > 0:
+		overload_cooldown_remaining -= 1
+	
+
+func use_overload() -> void:
+	
+	if battle_over:
+		return
+		
+	if overload_cooldown_remaining > 0:
+		status_label.text = "Overload is on cooldown!"
+		return
+	
+	# Actual skill
+	
+	enemy_hp -= player_data.overload_damage
+	enemy_hp = max(enemy_hp, 0)
+	
+	update_ui()
+	
+	if enemy_hp <= 0:
+		#Enemy dead
+		set_enemy_dead()
+		end_battle("Electrifying Victory!")
+		return
+	
+	overload_cooldown_remaining = player_data.overload_cooldown
+	enemy_turn()
+	
 func enemy_turn() -> void:
 	if battle_over:
 		return
@@ -123,7 +124,8 @@ func enemy_turn() -> void:
 	var damage: int = ENEMY_ATTACK_DAMAGE
 
 	if player_guarding:
-		damage = GUARDED_DAMAGE
+		damage -= player_data.stabilize_damage_reduction
+		damage = max(damage, 0)
 		player_guarding = false
 
 	player_hp -= damage
@@ -137,46 +139,49 @@ func enemy_turn() -> void:
 		set_player_dead()
 		end_battle("Defeat!")
 		return
-
+		
+	update_cooldowns()
 	status_label.text = "Player Turn"
 
 func end_battle(result_text: String) -> void:
 	battle_over = true
 	status_label.text = result_text
-	attack_button.disabled = true
-	guard_button.disabled = true
-	skill_button.disabled = true
+	pulse_button.disabled = true
+	stabilize_button.disabled = true
+	overload_button.disabled = true
 
 func reset_battle() -> void:
-	player_hp = STARTING_HP
+	player_hp = player_data.max_hp
+	player_energy = 0
 	enemy_hp = 10
-	player_mana = STARTING_MANA
+	overload_cooldown_remaining = 0
+	
 	
 	player_guarding = false
 	battle_over = false
-
-	status_label.text = "Player Turn"
-	attack_button.disabled = false
-	guard_button.disabled = false
-	skill_button.disabled = false
-	fireball_sprite.visible = false
-	
 	
 	set_player_alive()
 	set_enemy_alive()
+
+	status_label.text = "Player Turn"
+	pulse_button.disabled = false
+	stabilize_button.disabled = false
+	overload_button.disabled = false
+	# fireball_sprite.visible = false
 	
 	update_ui()
 
-func _on_attack_button_pressed() -> void:
-	print("Attack button pressed")
-	player_attack()
+func _on_pulse_button_pressed() -> void:
+	print("Pulse button pressed")
+	use_pulse()
 
-func _on_guard_button_pressed() -> void:
-	print("Guard button pressed")
-	player_guard()
+func _on_stabilize_button_pressed() -> void:
+	print("Stabilize button pressed")
+	use_stabilize()
+
+func _on_overload_button_pressed() -> void:
+	use_overload()
 
 func _on_reset_button_pressed() -> void:
 	reset_battle()
 	
-func _on_skill_button_pressed() -> void:
-	player_skill_fireball()
---
+++ lajada-e-luan/scripts/data/character_data.gd
@@ -0,0 +1,16 @@
+class_name CharacterData
+extends Resource
+
+@export var character_name: String = ""
+
+@export var max_hp: int = 10
+@export var max_energy: int = 100
+
+@export var pulse_damage: int = 3
+@export var overload_damage: int = 6
+@export var overload_cooldown: int = 2
+
+@export var stabilize_damage_reduction: int = 2
+
+@export var ultimate_damage: int = 10
+@export var ultimate_energy_cost: int = 100
