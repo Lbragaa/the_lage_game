8a3e842dd102f8a3febbcfb08b64fbba9bd71acd
Luan Braga <luancarlosalm@gmail.com>
2026-03-30 23:29:19 -0300

    First base kit, Arden Vale, added to the game

--- lajada-e-luan/scripts/battle/battle_controller.gd
+++ lajada-e-luan/scripts/battle/battle_controller.gd
@@ -3,15 +3,13 @@ extends Node2D
 @export var player_data: CharacterData
 
 #Texture
-const PLAYER_ALIVE_TEXTURE = preload("res://assets/characters/Sexo2_player.png")
+const PLAYER_ALIVE_TEXTURE = preload("res://assets/characters/ArdenVale_thegame.png")
 const PLAYER_DEAD_TEXTURE = preload("res://assets/characters/heroicaido.png")
 const ENEMY_ALIVE_TEXTURE = preload("res://assets/characters/Sexo2_enemy.png")
 const ENEMY_DEAD_TEXTURE = preload("res://assets/characters/enemy_cuiado.png")
 
-
 const ENEMY_ATTACK_DAMAGE := 3
 const GUARDED_DAMAGE := 1
-
 
 var player_hp: int
 var player_energy: int = 0
@@ -30,7 +28,10 @@ var overload_cooldown_remaining: int = 0
 @onready var overload_button = $UI/OverloadButton
 @onready var player_sprite = $UI/PlayerSprite
 @onready var enemy_sprite = $UI/EnemySprite
-@onready var fireball_sprite = $UI/FireballSprite
+# I know it is named fireball right now, i was just lazy to change to pulse ball in the rest of the code
+@onready var fireball_sprite = $UI/PulseballSprite
+@onready var discharge_button = $UI/DischargeButton
+@onready var energy_bar = $UI/EnergyBar
 
 # Functions to change the current sprite texture
 func set_player_alive() -> void:
@@ -45,13 +46,57 @@ func set_enemy_alive() -> void:
 func set_enemy_dead() -> void:
 	enemy_sprite.texture = ENEMY_DEAD_TEXTURE
 
+# Helper functions for energy related stuff
+
+func play_pulse_animation() -> void:
+	fireball_sprite.visible = true
+	fireball_sprite.global_position = player_sprite.global_position
+
+	var tween = create_tween()
+	tween.tween_property(
+		fireball_sprite,
+		"global_position",
+		enemy_sprite.global_position,
+		0.8
+	)
+
+	await tween.finished
+	fireball_sprite.visible = false
+
+func update_energy_bar() -> void:
+	for child in energy_bar.get_children():
+		child.queue_free()
+
+	for i in range(player_data.max_energy):
+		var square := ColorRect.new()
+		square.custom_minimum_size = Vector2(18, 18)
+
+		if i < player_energy:
+			square.color = Color(0.2, 0.5, 1.0, 1.0)
+		else:
+			square.color = Color(0.15, 0.15, 0.15, 0.6)
+
+		energy_bar.add_child(square)
+	
+func gain_energy(amount: int) -> void:
+	player_energy += amount
+	player_energy = min(player_energy, player_data.max_energy)
+
+func spend_energy(amount: int) -> bool:
+	if player_energy < amount:
+		return false
+	player_energy -= amount
+	return true
+
 func _ready() -> void:
 	reset_battle()
 
 func update_ui() -> void:
 	player_hp_label.text = "Player HP: " + str(player_hp)
 	enemy_hp_label.text = "Enemy HP: " + str(enemy_hp)
-	energy_label.text = "Energy Level: " + str(player_data.max_energy)
+	energy_label.text = "Energy Level: " + str(player_energy) + "/" + str(player_data.max_energy)
+	
+	update_energy_bar()
 	
 	if overload_cooldown_remaining > 0:
 		overload_button.text = "Overload (" + str(overload_cooldown_remaining) + ")"
@@ -61,7 +106,10 @@ func update_ui() -> void:
 func use_pulse() -> void:
 	if battle_over:
 		return
-
+	
+	gain_energy(player_data.pulse_energy_gain)
+	
+	play_pulse_animation()
 	enemy_hp -= player_data.pulse_damage
 	enemy_hp = max(enemy_hp, 0)
 
@@ -101,6 +149,9 @@ func use_overload() -> void:
 		status_label.text = "Overload is on cooldown!"
 		return
 	
+	if not spend_energy(player_data.overload_energy_cost):
+		status_label.text = "Not enough energy for Overload!"
+		return
 	# Actual skill
 	
 	enemy_hp -= player_data.overload_damage
@@ -116,7 +167,29 @@ func use_overload() -> void:
 	
 	overload_cooldown_remaining = player_data.overload_cooldown
 	enemy_turn()
+
+func use_discharge() -> void:
 	
+	if battle_over:
+		return
+		
+	if not spend_energy(player_data.ultimate_energy_cost):
+		status_label.text = "Not enough energy to use Discharge!"
+		return
+	
+	#Went through
+	
+	enemy_hp -= player_data.ultimate_damage
+	enemy_hp = max(enemy_hp, 0)
+	
+	status_label.text = "You used Discharge!"
+	update_ui()
+	
+	if enemy_hp <= 0:
+		set_enemy_dead()
+		end_battle("Victory!")
+		return
+
 func enemy_turn() -> void:
 	if battle_over:
 		return
@@ -127,6 +200,8 @@ func enemy_turn() -> void:
 		damage -= player_data.stabilize_damage_reduction
 		damage = max(damage, 0)
 		player_guarding = false
+		
+		gain_energy(player_data.stabilze_energy_gain)
 
 	player_hp -= damage
 	player_hp = max(player_hp, 0)
@@ -149,6 +224,8 @@ func end_battle(result_text: String) -> void:
 	pulse_button.disabled = true
 	stabilize_button.disabled = true
 	overload_button.disabled = true
+	discharge_button.disabled = true
+
 
 func reset_battle() -> void:
 	player_hp = player_data.max_hp
@@ -167,9 +244,10 @@ func reset_battle() -> void:
 	pulse_button.disabled = false
 	stabilize_button.disabled = false
 	overload_button.disabled = false
-	# fireball_sprite.visible = false
+	discharge_button.disabled = false
 	
 	update_ui()
+
 
 func _on_pulse_button_pressed() -> void:
 	print("Pulse button pressed")
@@ -182,6 +260,9 @@ func _on_stabilize_button_pressed() -> void:
 func _on_overload_button_pressed() -> void:
 	use_overload()
 
+func _on_discharge_button_pressed() -> void:
+	use_discharge()
+	
 func _on_reset_button_pressed() -> void:
 	reset_battle()
 	
--- lajada-e-luan/scripts/data/character_data.gd
+++ lajada-e-luan/scripts/data/character_data.gd
@@ -4,13 +4,16 @@ extends Resource
 @export var character_name: String = ""
 
 @export var max_hp: int = 10
-@export var max_energy: int = 100
+@export var max_energy: int = 10
 
 @export var pulse_damage: int = 3
 @export var overload_damage: int = 6
 @export var overload_cooldown: int = 2
 
+@export var pulse_energy_gain: int = 2
+@export var stabilze_energy_gain: int = 3
+@export var overload_energy_cost: int = 4
+@export var ultimate_energy_cost: int = 10
 @export var stabilize_damage_reduction: int = 2
 
 @export var ultimate_damage: int = 10
-@export var ultimate_energy_cost: int = 100
